6d402e932772b33c5a4b9a544e12642d53ddfb48
st01tkh <st01tkh@gmail.com>
2016-04-02 07:43:58 +0400

    Added ignore list

--- download.sh
+++ download.sh
@@ -5,8 +5,8 @@ downloadSelSize() {
   size="$1"
   mirror="$2"
   [ -n "$mirror" ] || mirror=$defaultMirror
-  urlPath="$thisDir/url-${mirror}-${size}MiB.txt"
-  logPath="$thisDir/wget-download-${mirror}-${size}MiB.log"
+  urlPath="$thisDir/url-${mirror}-${size}.txt"
+  logPath="$thisDir/wget-download-${mirror}-${size}.log"
   if [ ! -e "$urlPath" ]; then
     echo "ERROR: no such file $urlPath"
     return 20
@@ -14,13 +14,11 @@ downloadSelSize() {
   time wget -i "$urlPath" -o "$logPath" -O /dev/null
 }
 case "$1" in 
-  581) downloadSelSize "$1" ;;
-  180) downloadSelSize "$1" ;;
-  7) downloadSelSize "$1" ;;
+  581M|180M|7M|1M|57K) downloadSelSize "$1" ;;
   clear) find "$thisDir" -mindepth 1 -maxdepth 1 -type f -name 'wget-download-*-*.log' -delete ;;
   *)
-    echo "Usage: $(basename "$0") <581|180|7|clear> [<yandex>]"
-    echo "       <581|180|7|clear> - Download 581, 180, 7 Mib or clear logs"
+    echo "Usage: $(basename "$0") <581M|180M|7M|1M|57K|clear> [<yandex>]"
+    echo "       <581M|180M|7M|1M|57K|clear> - Download 581 Mib, 180 MiB, 7 MiB, 1 MiB, 57 KiB or clear logs"
     echo "       [<yandex>] - optional - Set mirror"
     exit 0
     ;;
